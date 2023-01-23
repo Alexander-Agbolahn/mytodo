0bb45f8b256e450a8f58bd76851f9b1d60a7e068
Alexander Oyedele <oyedelealexander98@gmail.com>
2023-01-23 06:44:59 -0800

    form validation with js remove chaining

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let posts = document.getElementById("posts");
 
-form?.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("button clicked");
   
